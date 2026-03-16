53c13f141fe52a09a8b8dfde924f379ce9325da4
Szymon Barłoga <szynutix@protonmail.com>
2026-03-16 09:49:30 +0000

    Test

--- SystemPrzychodznia/Form1.Designer.cs
+++ SystemPrzychodznia/Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Name = "Form1";
+            Text = "System Przychodnia";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
